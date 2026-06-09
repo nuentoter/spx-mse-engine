b0e833843fa7d43cbfaa5befb940c48665f986d5
nuentoter <nuentoter@gmail.com>
2026-06-09 07:27:57 -0400

    Update run.py

--- run.py
+++ run.py
@@ -1,12 +1,18 @@
+from time import sleep
+
 from spx.parser import parse
 from spx.runtime import Runtime
+
 
 def main():
     data = parse("game/forest.spx")
 
     runtime = Runtime(data)
 
-    runtime.render()
+    while True:
+        runtime.tick()
+        sleep(1)
+
 
 if __name__ == "__main__":
     main()
