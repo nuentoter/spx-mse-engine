93314b3b4c8cc14489ee5661e0adc2eea488db1c
nuentoter <nuentoter@gmail.com>
2026-06-09 18:46:25 -0400

    Update run.py

--- run.py
+++ run.py
@@ -1,18 +1,5 @@
-from time import sleep
-
-from spx.parser import parse
-from spx.runtime import Runtime
-
-
-def main():
-    data = parse("game/forest.spx")
-
-    runtime = Runtime(data)
-
-    while True:
-        runtime.tick()
-        sleep(1)
-
+from spx.parser import parse_spx
 
 if __name__ == "__main__":
-    main()
+    world = parse_spx("game/forest.spx")
+    print(world)
